5e507d91332dc6c7ad705f4ecf78e979aeb6cd56
Jon Lu <jon.lu@weightwatchers.com>
2019-12-29 23:46:24 -0500

    Progress

--- ARStuff/ViewController.swift
+++ ARStuff/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        
+        guard ARFaceTrackingConfiguration.isSupported else { fatalError() }
+        sceneView.delegate = self
+        
+        setupViews()
+        setupConstraints()
     }
 
+    let sceneView = ARSCNView()
 
+    private func setupViews() {
+        view.addSubview(sceneView)
+    }
+    
+    private func setupConstraints() {
+        sceneView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            sceneView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            sceneView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            sceneView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARFaceTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
 }
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = sceneView.device else { return nil }
+        
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        
+        return node
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let faceGeometry = node.geometry as? ARSCNFaceGeometry else { return }
+        faceGeometry.update(from: faceAnchor.geometry)
+    }
+}
